aba575db888442f0d3d0ffac99886f7c5ef1bc45
saraesabbagh <sarasabbagh@hotmail.co.uk>
2023-10-13 17:58:11 +0100

    completed most of the text and edits just need final confirmation

--- src/App.js
+++ src/App.js
@@ -1,71 +1,174 @@
-import logo from './logo.svg';
-import './App.css';
-import bench from './images/bench.jpg'
-import donate from './images/donate.jpg'
-import volunteer from "./images/volunteer.jpg"
+import "./App.css";
+import bench from "./images/bench.jpg";
+import donate from "./images/donate.jpg";
+import volunteer from "./images/volunteer.jpg";
 
 function App() {
+  const navContainer = document.querySelector(".nav-container");
+
+  // Add a scroll event listener to the window
+  window.addEventListener("scroll", () => {
+    // Get the current scroll position
+    const scrollY = window.scrollY;
+
+    // Set a threshold value for the scroll position (adjust as needed)
+    const scrollThreshold = 100;
+
+    // Calculate the opacity based on the scroll position
+    const opacity = Math.min(scrollY / scrollThreshold, 0.5);
+
+    // Set the background color to translucent white
+    navContainer.style.background = `rgba(255, 255, 255, ${opacity})`;
+  });
   return (
     <div className="App">
-      <div className='hero-container'>
-        <div className='nav-container'>
-        <nav>
-          <span>
-            <ul>
-              <li>About</li>
-              <li>Donate</li>
-              <li>Volunteer</li>
-              <li>Contact</li>
-            </ul>
+      <div className="hero-container">
+        <div className="nav-container">
+          <nav>
+            <span>
+              <ul>
+                <li>
+                  <a href="#donate">Donate</a>
+                </li>
+                <li>
+                  <a href="#about">About</a>
+                </li>
+                <li>
+                  <a href="#volunteer">Tea Parties</a>
+                </li>
+                <li>
+                  <a href="#contact">Contact</a>
+                </li>
+              </ul>
             </span>
-        </nav>
+          </nav>
         </div>
-        
-        <div className='hero-content'>
+
+        <div className="hero-content">
           <h4>Welcome To</h4>
-          <div className='hero-title'>
+          <div className="hero-title">
             <h1>HGS</h1>
             <h2>Rose Garden</h2>
           </div>
         </div>
       </div>
-      <div className='about-container'>
-        <h3 className='about-title'>What We do</h3>
-        <div className='about-content'>
-          <img src={bench} alt="bench in garden" style={{ width: '400px', height: 'auto' }} />
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error.
-
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error, h
+      <div id="donate" className="donate-container">
+        <div className="donate-image">
+          <img
+            src={donate}
+            alt="bench in garden"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </div>
+        <div className="donate-text">
+          <h3 className="donate-title">Donate</h3>
+          <h5 className="donate-subtitle">Please Support Us;</h5>
+          <p>
+            Could you please support our work by making a much-needed donation.
+            <br />
+            Our bank details are:
+            <br />
+            Northway Gardens Organisation (NGO)
+            <br />
+            NATWEST Account: 66631904
+            <br />
+            Sort: 51 - 50 -11
+            <br />A Cheque can also be sent to;
+            <br />
+            NGO c/o 69 Brookland Rise NW11 6DT
+            <br />
+            0208 458 5313
+            <br />
+            ingrambluebird@hotmail.com
           </p>
         </div>
       </div>
-      <div className='come-explore-container'>
-        <h2 className='come-explore-title'>Come Explore</h2>
-      </div>
-      <div className='donate-container'>
-      <h3 className='donate-title'>Donate</h3>
-        <div className='donate-content'>
-          <img src={donate} alt="bench in garden" style={{ width: '400px', height: 'auto' }} />
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error.
-
+      <div id="about" className="about-container">
+        <div className="about-text">
+          <h3 className="about-title">What We Do</h3>
+          <p>
+            Northway Gardens is an historic 120-year-old park in Hampstead
+            Garden Suburb. Local volunteers have been restoring the Northway
+            Rose Gardens since October 2014 as well as the whole ten acres of
+            the park. (Including Fletchers Gardens, the park opposite Cafe
+            Toulous NW11 6NY)
           </p>
         </div>
-
-      </div>
-      <div className='volunteer-container'>
-      <h3 className='volunteer-title'>Volunteer</h3>
-        <div className='volunteer-content'>
-          <img src={volunteer} alt="bench in garden" style={{ width: '400px', height: 'auto' }} />
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia, molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur error.
-          </p>
+        <div className="about-image">
+          <img
+            src={bench}
+            alt="bench in garden"
+            style={{ width: "100%", height: "auto" }}
+          />
         </div>
       </div>
-      <div className='lovehgs-container'>
-      <h2 className='lovehgs-title'>Love HGS</h2>
+      <div className="come-explore-container">
+        <h2 className="come-explore-title">Come</h2>
+        <h2 className="come-explore-title"> Explore</h2>
       </div>
-      <div className='contact-container'>
-        <h4 className='contact-email'>Email@memailaddres.com</h4>
-        <h2 className='contact-title'>Get in Touch</h2>
+
+      <div id="volunteer" className="volunteer-container">
+        <div className="volunteer-text">
+          <h3 className="volunteer-title">Tea Parties</h3>
+          <div className="volunteer-paragraphs">
+            <p>
+              Every Thursday (not winter) we have Tea Mornings in the park where
+              the community can get together and enjoy the park tranquility and
+              socialize.
+            </p>
+            <p>
+              Wednesday is a community Tea Morning in Romeo and Juliet Cafe in
+              the Market Place.
+            </p>
+            <p>
+              We have also had parties for dogs and children's birthdays and for
+              HGS Elders.
+            </p>
+          </div>
+        </div>
+        <div className="volunteer-image">
+          <img
+            src={volunteer}
+            alt="bench in garden"
+            style={{ width: "100%", height: "auto" }}
+          />
+        </div>
+      </div>
+      <div className="lovehgs-container">
+        <h2 className="lovehgs-title">Love HGS</h2>
+      </div>
+      <div id="contact" className="contact-container">
+        <div className="contact-info">
+          <h2 className="contact-title">Keep up </h2>
+          <h2 className="contact-title">to date on </h2>
+          <h2 className="contact-title"> Facebook</h2>
+        </div>
+        <div className="contact-social">
+          <a
+            className="contact-social"
+            href="https://www.facebook.com/groups/337257630584408"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p>HAMPSTEAD GARDEN SUBURB RESIDENTS</p>
+          </a>
+          <a
+            className="contact-social"
+            href="https://www.facebook.com/profile.php?id=100064469361231"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p>HGS Rose Gardens</p>
+          </a>
+          <a
+            className="contact-social"
+            href="https://www.facebook.com/profile.php?id=100082580617557"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <p>Friends of Hampstead Garden Suburb</p>
+          </a>
+        </div>
       </div>
     </div>
   );
